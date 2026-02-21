b2f406648ab005eab7db75819dea0e6077929470
thiago <thiagocontateste2025@gmail.com>
2026-02-21 14:06:53 -0300

    Inicio do projeto

--- app/src/main/java/com/example/admme/ui/ClientesFragment.java
+++ app/src/main/java/com/example/admme/ui/ClientesFragment.java
@@ -1,4 +1,4 @@
-package com.example.admme;
+package com.example.admme.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.admme.R;
 
 /**
  * A simple {@link Fragment} subclass.
